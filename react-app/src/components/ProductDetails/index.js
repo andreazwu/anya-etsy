f99e507be8137ec14e122839adb38d7be4b79ced
Andrea Wu <angeeya.wu@gmail.com>
2022-11-17 22:21:57 -0600

    productId product.id

--- react-app/src/components/ProductDetails/index.js
+++ react-app/src/components/ProductDetails/index.js
@@ -44,7 +44,7 @@ const ProductDetails = () => {
                 return history.push('/')
             }
         console.log("-------ProductDetail---BEFORE---dispatchThunk-----")
-        console.log(`productId: ${productId} is a ${typeof(productId)}`)
+        console.log(`productId: +${productId} is a ${typeof(+productId)}`)
         console.log(`product.id: ${product.id} is a ${typeof(product.id)}`)
         await dispatch(addCartItemThunk(product.id, quantity))
         console.log("-------ProductDetail---AFTER---dispatchThunk-----")
